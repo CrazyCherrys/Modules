a35e639d99b5d3266b36f614db6f32ce4ed9a097
cherry <2@qq.com>
2025-05-17 17:06:00 +0800

    初始化

---
+++ Module-demo/calculate_pannel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module_demo
+{
+    public partial class calculate_pannel : Form
+    {
+
+        //基准价格
+        double base_price_1K = 0.002;
+        double base_price_1M = 2;
+
+        public calculate_pannel()
+        {
+            InitializeComponent();
+
+            units.Items.Add("1M,即百万Token");
+            units.Items.Add("1K，即1K Tokens");
+            units.SelectedIndex = 0;
+
+            // 添加 RadioButton 的选择改变事件处理
+            radioButton1.CheckedChanged += RadioButton_CheckedChanged;
+            radioButton2.CheckedChanged += RadioButton_CheckedChanged;
+
+
+        }
+
+        private void RadioButton_CheckedChanged(object sender , EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null && radioButton.Checked)
+            {
+                if (radioButton == radioButton1)
+                {
+                    enter_price_title.Text = "官方每单位输入价格 (美元)：";
+                    output_price_title.Text = "官方每单位输出价格 (美元)：";
+                }
+                else if (radioButton == radioButton2) 
+                {
+                    enter_price_title.Text = "官方每单位输入价格 (人民币)：";
+                    output_price_title.Text = "官方每单位输出价格 (人民币)：";
+                }
+
+            }
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            //汇率
+            double exchange_rate = 7.3;
+
+            if (radioButton1.Checked)
+            {
+                if (units.Text.Contains("1M"))
+                {
+                    //转换输入价格为double类型
+                    double input_prices = double.Parse(input_price.Text);
+                    //计算倍率
+                    double ins = input_prices / base_price_1M;
+
+                    //转换输出价格为double类型
+                    double outut_prices = double.Parse(output_price.Text);
+                    //计算倍率
+                    double outs = outut_prices / input_prices;
+
+                    //修改属性
+                    model_ratio.Text = "模型倍率为：" + ins;
+                    complete_ratio.Text = "补全倍率为：" + outs;
+                }
+                else if (units.Text.Contains("1K"))
+                {
+                    //转换输入价格为double类型
+                    double input_prices = double.Parse(input_price.Text);
+                    //计算倍率
+                    double ins = input_prices / base_price_1K;
+
+                    //转换输出价格为double类型
+                    double outut_prices = double.Parse(output_price.Text);
+                    //计算倍率
+                    double outs = outut_prices / input_prices;
+
+                    //修改属性
+                    model_ratio.Text = "模型倍率为：" + ins;
+                    complete_ratio.Text = "补全倍率为：" + outs;
+                }
+            } else if (radioButton2.Checked) 
+            {
+                if (units.Text.Contains("1M"))
+                {
+                    //转换输入价格为double类型
+                    double input_prices = double.Parse(input_price.Text);
+                    //计算倍率
+                    double ins = (input_prices / exchange_rate) / base_price_1M;
+
+                    //转换输出价格为double类型
+                    double outut_prices = double.Parse(output_price.Text);
+                    //计算倍率
+                    double outs = outut_prices / input_prices;
+
+                    //修改属性
+                    model_ratio.Text = "模型倍率为：" + ins;
+                    complete_ratio.Text = "补全倍率为：" + outs;
+                }
+                else if (units.Text.Contains("1K"))
+                {
+                    //转换输入价格为double类型
+                    double input_prices = double.Parse(input_price.Text);
+                    //计算倍率
+                    double ins = (input_prices / exchange_rate) / base_price_1K;
+
+                    //转换输出价格为double类型
+                    double outut_prices = double.Parse(output_price.Text);
+                    //计算倍率
+                    double outs = outut_prices / input_prices;
+
+                    //修改属性
+                    model_ratio.Text = "模型倍率为：" + ins;
+                    complete_ratio.Text = "补全倍率为：" + outs;
+                }
+            }
+
+        }
+    }
+}
---
+++ Module-demo/login_pannel.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Module_demo
+{
+    partial class loginin_pannel
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.Label();
+            this.passwd = new System.Windows.Forms.Label();
+            this.username_text = new System.Windows.Forms.TextBox();
+            this.passwd_text = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(77, 151);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(71, 18);
+            this.username.TabIndex = 0;
+            this.username.Text = "用户名:";
+            // 
+            // passwd
+            // 
+            this.passwd.AutoSize = true;
+            this.passwd.Location = new System.Drawing.Point(77, 221);
+            this.passwd.Name = "passwd";
+            this.passwd.Size = new System.Drawing.Size(62, 18);
+            this.passwd.TabIndex = 1;
+            this.passwd.Text = "密码：";
+            // 
+            // username_text
+            // 
+            this.username_text.Location = new System.Drawing.Point(202, 148);
+            this.username_text.Name = "username_text";
+            this.username_text.Size = new System.Drawing.Size(199, 28);
+            this.username_text.TabIndex = 2;
+            // 
+            // passwd_text
+            // 
+            this.passwd_text.Location = new System.Drawing.Point(202, 211);
+            this.passwd_text.Name = "passwd_text";
+            this.passwd_text.Size = new System.Drawing.Size(199, 28);
+            this.passwd_text.TabIndex = 3;
+            // 
+            // btn_login
+            // 
+            this.btn_login.AutoSize = true;
+            this.btn_login.Location = new System.Drawing.Point(165, 438);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(173, 110);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "登录";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // loginin_pannel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 743);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.passwd_text);
+            this.Controls.Add(this.username_text);
+            this.Controls.Add(this.passwd);
+            this.Controls.Add(this.username);
+            this.Name = "loginin_pannel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label passwd;
+        private System.Windows.Forms.TextBox username_text;
+        private System.Windows.Forms.TextBox passwd_text;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
